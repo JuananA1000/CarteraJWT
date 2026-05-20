4f51015f947359dfede97c9dd3ba7c36c3fccb40
Juanan <juanantonioamillopez@gmail.com>
2026-05-20 15:50:41 +0200

    PROJECT: Cartera de credenciales con JWT

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div id='center'>
+      <h1 className='title'>Cartera de credenciales con JWT</h1>
+    </div>
+  );
+}
+
+export default App;
