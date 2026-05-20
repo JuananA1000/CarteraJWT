9b8d0c82b12e35a1ec26e1d6778f5da0c7e86ca9
Juanan <juanantonioamillopez@gmail.com>
2026-05-20 17:22:38 +0200

    empezamos de cero

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import { useState } from 'react';
+
 import './App.css';
 
 function App() {
+  const [refrescarWallet, setRefrescarWallet] = useState(false);
+
   return (
     <div id='center'>
       <h1 className='title'>Cartera de credenciales con JWT</h1>
